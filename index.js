a616baea216c4df509b25b12ee1f7f7f997266b4
Valeria Viana Gusmao <vvg@zerotech.ru>
2017-07-31 16:37:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var brain = require('brain.js');
+var langJson = require('./lang.json');
+var fs = require('fs');
+
+var net = new brain.recurrent.LSTM();
+fs.open('./net.json', 'r', (err, fd) => {
+  if (err) {
+    net.train(langJson, {
+      log: true,
+      iterations: 400
+    });
+    var json = net.toJSON();
+    fs.writeFileSync('./net.json', JSON.stringify(json));
+  } else {
+    net.fromJSON(require('./net.json'));
+  }
+  console.log('children',' => ',net.run('children'));
+  console.log('children of blood',' => ',net.run('children of blood'));
+  console.log('children of blood and of the sun',' => ',net.run('children of blood and of the sun'));
+  console.log('children of Medivh',' => ',net.run('children of medivh'));
+});
