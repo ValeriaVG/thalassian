a26cc2a93999fb1fd9ed6fc4a31525d0e12755ec
Valeria <valeria.viana.gusmao@gmail.com>
2017-08-01 01:02:36 +0300

    Re-trained
    Changed data a bit - tryouts

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ fs.open('./docs/net.json', 'r', (err, fd) => {
   if (err) {
     net.train(langJson, {
       log: true,
-      errorThresh:1.25
+      errorThresh: 1.3
     });
     var json = net.toJSON();
     fs.writeFileSync('./docs/net.json', JSON.stringify(json));
   } else {
     net.fromJSON(require('./docs/net.json'));
   }
-  console.log('children',' => ',net.run('children'));
-  console.log('children of blood',' => ',net.run('children of blood'));
-  console.log('children of blood and of the sun',' => ',net.run('children of blood and of the sun'));
-  console.log('children of Medivh',' => ',net.run('children of medivh'));
+  console.log('children', ' => ', net.run('children'));
+  console.log('children of blood', ' => ', net.run('children of blood'));
+  console.log('children of blood and of the sun', ' => ', net.run('children of blood and of the sun'));
+  console.log('children of Medivh', ' => ', net.run('children of medivh'));
 });
